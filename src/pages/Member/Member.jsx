0af9dd5b87ae9b5fe7c8313333d1819d9a083703
Christopher Burnette <chrisburnette083@gmail.com>
2023-04-14 14:56:20 +0500

    rendor members data from mock json

--- src/pages/Member/Member.jsx
+++ src/pages/Member/Member.jsx
@@ -1,10 +1,33 @@
 import './style.css';
+const memberData = require('./../../data/members.json');
 
 const Member = () => {
     return (
-        <nav>
-            Member
-        </nav>
+        <div className='member-container'>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>ID</th>
+                        <th>Email</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Team Name</th>
+                    </tr>
+                    {
+                        memberData?.members?.length > 0 &&
+                        memberData.members.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.id}</td>
+                                <td>{item.email}</td>
+                                <td>{item.first_name}</td>
+                                <td>{item.last_name}</td>
+                                <td>{item?.team?.name}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
     )
 }
 export default Member;

25f3d0c06552f51247a8b6f742f1eaa9d91e3992
Christopher Burnette <chrisburnette083@gmail.com>
2023-04-14 16:37:05 +0500

    added naviation links in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
-import './style.css';
-
+import styles from './Header.module.css';
+import { useNavigate } from 'react-router-dom'
 const Header = () => {
+    const navigate = useNavigate();
+
     return (
-        <nav>
-            Header
-        </nav>
+        <div className={styles.topnav}>
+            <a onClick={() => navigate('/member')}>Member</a>
+            <a onClick={() => navigate('/team')}>Team</a>
+            <a onClick={() => navigate('/todo')}>Todo</a>
+        </div>
     )
 }
 export default Header;

3cc494f9ef5b21892f12d9627968e163fe3d5ca6
Christopher Burnette <chrisburnette083@gmail.com>
2023-04-14 14:59:34 +0500

    rendor teams data from mock json

--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -1,10 +1,29 @@
 import './style.css';
+const data = require('./../../data/teams.json');
 
 const Team = () => {
     return (
-        <nav>
-            Team
-        </nav>
+        <div className='member-container'>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Count</th>
+                    </tr>
+                    {
+                        data?.teams?.length > 0 &&
+                        data.teams.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.id}</td>
+                                <td>{item.name}</td>
+                                <td>{item.member_count}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
     )
 }
 export default Team;

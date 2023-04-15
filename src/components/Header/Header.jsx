7fabbce13e98ed64cb7797747745350112734a46
Christopher Burnette <chrisburnette083@gmail.com>
2023-04-15 14:39:42 +0500

    applied active class in menu links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,18 @@
 import styles from './Header.module.css';
-import { useNavigate ,} from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom';
+
 const Header = () => {
     const navigate = useNavigate();
+    const location = useLocation();
+    const memberClass = location.pathname.match(/^\/members/) ? styles.active : "";
+    const teamClass = location.pathname.match(/^\/teams/) ? styles.active : "";
+    const todoClass = location.pathname.match(/^\/todos/) ? styles.active : "";
 
     return (
         <div className={styles.topnav}>
-            <a onClick={() => navigate('/members')}>Member</a>
-            <a onClick={() => navigate('/teams')}>Team</a>
-            <a onClick={() => navigate('/todos')}>Todo</a>
+            <a className={memberClass} onClick={() => navigate('/members')}>Member</a>
+            <a className={teamClass} onClick={() => navigate('/teams')}>Team</a>
+            <a className={todoClass} onClick={() => navigate('/todos')}>Todo</a>
         </div>
     )
 }

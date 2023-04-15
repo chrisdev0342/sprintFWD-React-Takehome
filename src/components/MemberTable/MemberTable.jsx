9daf9f32e76905fcc0ecb6606a759e17ef0fe762
Christopher Burnette <chrisburnette083@gmail.com>
2023-04-15 13:27:42 +0500

    created common component for member list

---
+++ src/components/MemberTable/MemberTable.jsx
@@ -0,0 +1,44 @@
+import './style.css';
+import { useNavigate } from 'react-router-dom'
+import PropTypes from 'prop-types';
+const memberData = require('./../../data/members.json');
+
+const MemberTable = (props) => {
+    const { teamId } = props;
+    const navigate = useNavigate();
+    const members = teamId ? memberData.members.filter(item => item.team.id == teamId) : memberData.members;
+    return (
+        <div className='member-container'>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>ID</th>
+                        <th>Email</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Team Name</th>
+                    </tr>
+                    {
+                        members?.length > 0 &&
+                        members.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.id}</td>
+                                <td>{item.email}</td>
+                                <td>{item.first_name}</td>
+                                <td>{item.last_name}</td>
+                                <td className={!teamId && 'link'} onClick={!teamId ? () => navigate(`/team-detail/${item.team.id}`) : undefined}>{item?.team?.name}</td>
+
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div >
+    )
+}
+
+MemberTable.propTypes = {
+    teamId: PropTypes.string,
+};
+
+export default MemberTable;
--- src/pages/Member/Member.jsx
+++ src/pages/Member/Member.jsx
@@ -1,33 +1,11 @@
-import './style.css';
-const memberData = require('./../../data/members.json');
+import MemberTable from '../../components/MemberTable/MemberTable';
 
 const Member = () => {
     return (
-        <div className='member-container'>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>ID</th>
-                        <th>Email</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Team Name</th>
-                    </tr>
-                    {
-                        memberData?.members?.length > 0 &&
-                        memberData.members.map((item, index) => (
-                            <tr key={index}>
-                                <td>{item.id}</td>
-                                <td>{item.email}</td>
-                                <td>{item.first_name}</td>
-                                <td>{item.last_name}</td>
-                                <td>{item?.team?.name}</td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
-        </div>
+        <MemberTable />
     )
 }
+
+
+
 export default Member;

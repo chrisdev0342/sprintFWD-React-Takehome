74b679f8a4a847533fefeb1b17cc5be29eaac699
Christopher Burnette <chrisburnette083@gmail.com>
2023-04-15 14:54:38 +0500

    applied lazy loaded routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,19 @@
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Member from './../pages/Member/Member';
-import Team from './../pages/Team/Team';
-import Todo from './../pages/Todo/Todo';
-import TeamDetails from './../pages/TeamDetails/TeamDetails';
+const Member = lazy(() => import('./../pages/Member/Member'));
+const Team = lazy(() => import('./../pages/Team/Team'))
+const Todo = lazy(() => import('./../pages/Todo/Todo'))
+const TeamDetails = lazy(() => import('./../pages/TeamDetails/TeamDetails'))
 
 const Router = () => (
-    <Routes>
-        <Route path="members" element={<Member />} />
-        <Route path="team-details/:id" element={<TeamDetails />} />
-        <Route path="teams" element={<Team />} />
-        <Route path="todos" element={<Todo />} />
-        <Route path="*" element={<p>There's nothing here: 404!</p>} />
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+            <Route path="members" element={<Member />} />
+            <Route path="team-details/:id" element={<TeamDetails />} />
+            <Route path="teams" element={<Team />} />
+            <Route path="todos" element={<Todo />} />
+            <Route path="*" element={<p className='text-align-center'>There's nothing here: 404!</p>} />
+        </Routes>
+    </Suspense>
 );
 export default Router;

a502678e3a0f3493198958dac5424b305e162366
Christopher Burnette <chrisburnette083@gmail.com>
2023-04-17 11:58:04 +0500

    implemented default route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Router from './routes';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function App() {
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    if (location.pathname === '/') {
+      navigate('/members')
+    }
+  }, [])
+  
   return (
     <div className="App">
       <Header />
